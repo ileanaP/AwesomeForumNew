76eb0c3b41696f390d91feff1ee75182a4a63ca1
IleanaP <ileana.profeanu@gmail.com>
2022-12-11 22:37:58 +0200

    login and register pages

---
+++ AwesomeForum/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using AwesomeForum.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace AwesomeForum.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginVM newLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newLogin);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterVM newRegister)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newRegister);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
